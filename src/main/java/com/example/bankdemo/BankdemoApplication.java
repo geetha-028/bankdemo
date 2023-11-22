183c402d62010789872d2d7b4ca92bf4f677e2c6
Geetha P <geetha@gmail.com>
2023-11-22 12:42:58 +0530

    update the code

--- src/main/java/com/example/bankdemo/BankdemoApplication.java
+++ src/main/java/com/example/bankdemo/BankdemoApplication.java
@@ -3,7 +3,6 @@ package com.example.bankdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.bankdemo.ctrl.StudentCtrl;
 import com.example.bankdemo.service.StudentServiceImpl;
@@ -13,10 +12,10 @@ public class BankdemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(BankdemoApplication.class, args);
-		
-//		StudentServiceImpl impl = context.getBean(StudentServiceImpl.class);
-//		System.out.println("impl name : "+impl.name);
-//		System.out.println("impl portNumber : "+impl.portNumber);
+
+		StudentServiceImpl impl = context.getBean(StudentServiceImpl.class);
+		System.out.println("impl name : " + impl.name);
+		System.out.println("impl portNumber : " + impl.portNumber);
 	}
 
 }
